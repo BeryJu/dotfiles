dfd9660ca55decd46d906b4cc825ff15adfc1830
Jens Langhammer <jens.langhammer@beryju.org>
2021-11-03 10:59:03 +0100

    add scripts

---
+++ run_onchange_after_10_fisher.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+type fisher &>/dev/null || curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install sentriz/fish-pipenv
+fisher install evanlucas/fish-kubectl-completions
+fisher install decors/fish-colored-man
+fisher install jethrokuan/z
+fisher install oh-my-fish/plugin-bang-bang
+fisher install PatrickF1/fzf.fish
